77a72ec4681c7747fce23e579cffcfe6ee1caf47
= <jimmy@robert.cloud>
2020-06-29 08:38:40 +0200

    Customize macos example toolbar.

--- packages/example/macos/Runner/MainFlutterWindow.swift
+++ packages/example/macos/Runner/MainFlutterWindow.swift
@@ -8,6 +8,13 @@ class MainFlutterWindow: NSWindow {
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
 
+    self.contentMinSize = NSSize(width: 640, height: 480)
+    
+    self.titlebarAppearsTransparent = true
+    self.titleVisibility = .hidden
+    self.isMovableByWindowBackground = true
+    self.styleMask.insert(NSWindow.StyleMask.fullSizeContentView)
+
     RegisterGeneratedPlugins(registry: flutterViewController)
 
     super.awakeFromNib()
