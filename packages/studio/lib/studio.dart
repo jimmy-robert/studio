4db336f6b4145c750fc53ab6c28f7ac319b1a364
= <jimmy@robert.cloud>
2020-06-30 20:57:29 +0200

    Update public API.

--- packages/studio/lib/studio.dart
+++ packages/studio/lib/studio.dart
@@ -1,5 +1,6 @@
-export 'src/core/provider/extension.dart';
+export 'src/core/app/app.dart';
+export 'src/core/lifecycle/lifecycle.dart';
 export 'src/core/provider/module.dart';
-export 'src/core/provider/provided.dart';
 export 'src/core/provider/provider.dart';
-export 'src/core/provider/resolver.dart';
+export 'src/core/resolver/resolver.dart';
+export 'src/core/studio/studio.dart';
