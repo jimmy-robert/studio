ffbc74ffc12865e18c9f8918e4e51f5bad8f1367
= <jimmy@robert.cloud>
2020-06-30 23:05:46 +0200

    Name should not be null in async action.

--- packages/studio/lib/src/core/reactive/async_action.dart
+++ packages/studio/lib/src/core/reactive/async_action.dart
@@ -1,5 +1,5 @@
 import 'package:mobx/mobx.dart';
 
-Future<T> runInAsyncAction<T>(Future<T> Function() callback, {String name}) {
+Future<T> runInAsyncAction<T>(Future<T> Function() callback, {String name = ''}) {
   return AsyncAction(name).run<T>(callback);
 }
