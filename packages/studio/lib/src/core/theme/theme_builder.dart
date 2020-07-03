b443c3fe4e9792636d5f5191bd1473ac7d6e7e42
= <jimmy@robert.cloud>
2020-07-03 09:04:23 +0200

    Init ThemeBuilder

---
+++ packages/studio/lib/src/core/theme/theme_builder.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../provider/provider.dart';
+import '../reactive/observer.dart';
+import 'theme_mode_controller.dart';
+
+class ThemeBuilder extends StatelessWidget {
+  final ThemeData Function(ThemeData theme) light;
+  final ThemeData Function(ThemeData theme) dark;
+  final Widget child;
+
+  const ThemeBuilder({this.light, this.dark, this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Observer.withContext((context) {
+      var mode = context.get<ThemeModeController>()?.mode?.value;
+
+      if (mode == null || mode == ThemeMode.system) {
+        final brightness = MediaQuery.platformBrightnessOf(context);
+        mode = brightness == Brightness.dark ? ThemeMode.dark : ThemeMode.light;
+      }
+
+      var theme = Theme.of(context, shadowThemeOnly: true);
+      theme ??= mode == ThemeMode.light ? ThemeData.light() : ThemeData.dark();
+
+      if (mode == ThemeMode.light && light != null) {
+        theme = light(theme);
+      } else if (mode == ThemeMode.dark && dark != null) {
+        theme = dark(theme);
+      }
+
+      return AnimatedTheme(
+        data: theme,
+        child: child,
+      );
+    });
+  }
+}
