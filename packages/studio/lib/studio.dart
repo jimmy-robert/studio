b34c6a84d73e98ff5d284c9a59cae0864c515ddb
Jimmy Robert <jimmy@robert.cloud>
2020-09-05 16:41:23 +0200

    Update main export

--- packages/studio/lib/studio.dart
+++ packages/studio/lib/studio.dart
@@ -4,12 +4,14 @@ export 'package:intl/intl.dart' hide TextDirection;
 
 export 'src/core/app/app.dart';
 export 'src/core/app/app_controller.dart';
-export 'src/core/lifecycle/lifecycle.dart';
 export 'src/core/injection/module.dart';
 export 'src/core/injection/provider.dart';
+export 'src/core/lifecycle/lifecycle.dart';
 export 'src/core/reactive/rx.dart';
-export 'src/core/theme/theme_controller.dart';
+export 'src/core/serializer/serializer.dart';
 export 'src/core/theme/platform_controller.dart';
+export 'src/core/theme/theme_controller.dart';
+export 'src/utils/cast.dart';
 export 'src/utils/duration.dart';
 export 'src/utils/navigator.dart';
 export 'src/utils/number.dart';
